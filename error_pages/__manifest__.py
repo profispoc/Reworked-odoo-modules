5e488713d1ffd3d6cf2e43016467813737c82419
Kaliuta <jumpvampir@gmail.com>
2022-10-10 14:27:53 +0300

    error pages uk translate

---
+++ error_pages/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020-Today TechKhedut.
+# Part of TechKhedut. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Error Pages - 400, 404, 403, & 500',
+    'category': 'Website',
+    'version': '15.0.1.1.1',
+    'author': 'TechKhedut Inc.',
+    'company': 'TechKhedut Inc.',
+    'maintainer': 'TechKhedut Inc.',
+    'website': "https://www.techkhedut.com",
+    'summary': 'Error pages 403, 404, 500, & others.',
+    'depends': ['website', 'http_routing', 'base'],
+    'data': [
+        'views/views.xml',
+    ],
+    'images': ['static/description/screenshot_2.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+}
