bc6fb871a66327a873f498c19c7402c1b75da598
Kaliuta <jumpvampir@gmail.com>
2022-10-12 09:10:26 +0300

    add website_sale_b2x_alt_price

--- website_snippet_carousel_product/__manifest__.py
+++ website_snippet_carousel_product/__manifest__.py
@@ -9,7 +9,19 @@
     "license": "LGPL-3",
     "summary": "Adds a new snippet to insert a carousel of products",
     "depends": ["website_sale"],
-    "data": ["templates/assets.xml", "templates/snippet.xml"],
+    "data": [
+        #"templates/assets.xml",
+        "templates/snippet.xml"
+    ],
     "installable": True,
     "maintainers": ["Tardo"],
+    'assets': {
+        'web.assets_frontend': [
+            'website_snippet_carousel_product/static/src/scss/s_product_carousel.scss',
+            'website_snippet_carousel_product/static/src/js/s_product_carousel_frontend.js',
+        ],
+        'website.assets_editor': [
+            'website_snippet_carousel_product/static/src/js/snippet.options.js',
+        ],
+    },
 }
