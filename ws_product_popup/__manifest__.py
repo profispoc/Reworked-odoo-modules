507548188f2b0cf4b3ce1d4ac749be889506ed19
Kaliuta <jumpvampir@gmail.com>
2022-10-12 09:55:39 +0300

    add ws_product_popup

---
+++ ws_product_popup/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Dynamic Product Pop-up",
+    "summary": """
+        Add dynamic product pop-up for easy viewing of products in shop
+    """,
+    "description": """
+        Add dynamic product pop-up for easy viewing of products in shop
+    """,
+    "author": "Peter M. Fam",
+    "website": "https://petermfam.me",
+    "license": "GPL-3",
+    "category": "Website",
+    "version": "0.1",
+    "depends": ["base", "website", "website_sale"],
+    "data": [],
+    "images": ["images/main_screenshot.gif"],
+    "assets":{
+        "web.assets_frontend":[
+            "ws_product_popup/static/src/js/dynamic_product_popup.js",
+        ],
+    },
+}
